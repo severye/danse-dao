7e6335fbe5b1bc49648bf632db0bfd7c65db3b7d
severye <severine.bouillet@decathlon.com>
2018-04-20 16:26:42 +0200

    connexion access token auth0

--- src/main/java/danse/config/DomainAndPersistenceConfig.java
+++ src/main/java/danse/config/DomainAndPersistenceConfig.java
@@ -7,17 +7,19 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableJpaRepositories(basePackages = { "danse.repositories","danse.security"})
+@EnableJpaRepositories(basePackages = { "danse.repositories"})
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "danse" })
-@EntityScan(basePackages = { "danse.entities","danse.security"})
+@EntityScan(basePackages = { "danse.entities"})
 @EnableTransactionManagement
+@Configuration
 public class DomainAndPersistenceConfig {
 
 	// la source de données MySQL
--- src/main/java/danse/security/UserRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package danse.security;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
-
-}
